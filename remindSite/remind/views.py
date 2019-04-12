81461ea5c94f7aa518e975ead262180ec4ec8792
HadisDaqiq <muhadisa93@gmail.com>
2019-04-12 17:47:54 -0400

    complete backend

--- remindSite/remind/views.py
+++ remindSite/remind/views.py
@@ -14,7 +14,6 @@ from remind.services import locationService
 
 class ReminderListView(ListView):
     model = Reminder
-    paginate_by=20
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
